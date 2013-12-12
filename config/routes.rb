a59659beb4d77c52f27fb539612f78927c341da1
Diego Barrera <dieg_goo@hotmail.com>
2013-12-11 19:56:44 -0500

    finish commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 PruebaRails::Application.routes.draw do
+  resources :microposts
+
+
+  resources :users
+
+
   get "welcome/index"
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131211180708) do
+ActiveRecord::Schema.define(:version => 20131212002549) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "welcomes", :force => true do |t|
     t.datetime "created_at", :null => false
